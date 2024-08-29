f32c962947f4a9bb39e045999e291432306abf8f
Abhishek <abhishekraghuvanshi52@gmail.com>
2024-08-29 16:45:00 +0530

    pushing

---
+++ Backend/config/database.js
@@ -0,0 +1,12 @@
+const  mongoose = require('mongoose')
+
+require('dotenv').config()
+
+exports.connect = () => {
+    mongoose.connect(process.env.MONGODB_URL, {
+    }).then(()=>console.log("DB Connected Successfully✅"))
+    .catch((error)=>{ 
+        console.log("this error occured"+ error)
+        process.exit(1)
+    })
+}
